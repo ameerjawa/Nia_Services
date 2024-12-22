6b100f0d5b9ef265cde5eb7c2975c280aed93b15
ameerjawa <ameer0233@gmail.com>
2024-12-21 19:41:54 -0800

    'test3'

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
-import emailjs from "emailjs-com";
 import React from "react";
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
+// const initialState = {
+//   name: "",
+//   email: "",
+//   message: "",
+// };
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  // const [{ name, email, message }, setState] = useState(initialState);
 
   // const handleChange = (e) => {
   //   const { name, value } = e.target;
   //   setState((prevState) => ({ ...prevState, [name]: value }));
   // };
-  const clearState = () => setState({ ...initialState });
+  // const clearState = () => setState({ ...initialState });
   
   
   // const handleSubmit = (e) => {
